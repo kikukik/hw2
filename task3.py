467bd15223d7930cdc3ab74eee2770a96a0a0f53
kikukik <66834065+kikukik@users.noreply.github.com>
2020-06-15 20:54:42 +0200

    3 und 4 py upload

---
+++ task3.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 17:32:56 2020
+
+@author: rinorcakaj
+"""
+#imports
+import math as m
+import numpy as np
+from matplotlib import pyplot as plt
+
+#data
+nonParamTrain = np.loadtxt("dataSets/nonParamTrain.txt")
+nonParamTest = np.loadtxt("dataSets/nonParamTest.txt")
+
+
+def histogram(data, bin_size):
+    # funktion selber geschrieben, aber passt das?
+    bins = np.arange(min(data), max(data) + bin_size, bin_size)
+    hist = np.zeros((len(bins),2))
+    hist[:,0] = bins
+    for i in range(len(hist)):
+        print(i)
+        for j in range(len(data)):
+            if hist[i,0] <= data[j] < hist[i+1,0]:
+                hist[i,1] = hist[i,1] + 1
+    hist[:,0] = hist[:,0] + 0.5 * bin_size
+    plt.bar(hist[:,0], hist[:,1], color="red", width=bin_size)
+    plt.plot(hist[:,0], hist[:,1])
+    # plt.hist(data, bins=np.arange(min(data), max(data) + bin_size, bin_size)) zur Überprüfung
+    plt.show()
+    
+def gaussian_kernel(data, x, sigma):
+    result = 0
+    for i in range(len(data)):
+        result += m.exp(-(x-data[i])**2 / (2 * sigma**2))
+    result = (result/(len(data) * m.sqrt(2 * m.pi * sigma**2)))
+    return result
+
+def log_likehood_gaussian(data, log_like_data, sigma):
+    result = 0
+    for i in range(len(log_like_data)):
+        result += np.log(gaussian_kernel(data, log_like_data[i], sigma))
+    return result
+
+def log_likehood_neighbor(data, log_like_data, K):
+    result = 0 
+    for i in range(len(log_like_data)):
+        result += m.log(k_nearest_neighbor(data, K, log_like_data[i]))
+    return result
+
+def k_nearest_neighbor(data,K,x):
+    values = np.column_stack((data, data))
+    values[:,0] = abs(values[:,0] - x)
+    values = values[values[:,0].argsort()]
+    result = values[K,0]
+    return (K / (result * len(data)))
+    
+
+if __name__ == "__main__":
+    # settings 3a)
+    hist_data = nonParamTrain
+    hist_bin_size = 0.3
+    
+    # 3a) Histogramme
+    histogram(hist_data, hist_bin_size)
+    
+    # # settings
+    # data = nonParamTrain
+    
+    # # plot densities
+    # x = np.linspace(-4,8,100)
+    # y = []
+    # for sigma in [0.03, 0.2, 0.8]:
+    #     for i in range(len(x)):
+    #         y.append(gaussian_kernel(data, x[i], sigma))
+    #     plt.plot(x,y)
+    #     y = []
+    # plt.show()
+    
+    # # log-likehood
+    
+    # for sigma in [0.03, 0.2, 0.8]:
+    #     log_likehood_solution = log_likehood_gaussian(data, data, sigma)
+    #     print("sigma:", sigma, "log_likehood:", log_likehood_solution)
+        
+        
+    # # 3c) K - Nearest - Neighbor
+    # x = np.linspace(-4,8,1200)
+    # y = []
+    # for K in [2, 8, 35]:
+    #     for i in range(len(x)):
+    #         y.append(k_nearest_neighbor(data,K,x[i]))
+    #     plt.plot(x,y)
+    #     y = []
+    # plt.show()
+    
+    # # 3d) Log Likehood 
+    
+    # for sigma in [0.03, 0.2, 0.8]:
+    #     log_likehood_solution = log_likehood_gaussian(data, nonParamTest , sigma)
+    #     print("sigma:", sigma, "log_likehood_gaussian:", log_likehood_solution)
+        
+    # for K in [2, 8, 35]:
+    #     log_likehood_solution = log_likehood_neighbor(data, nonParamTest, K)
+    #     print("sigma:", K, "log_likehood_neighbor:", log_likehood_solution)
+    
+    
+    
+    
---
+++ task4.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 14 20:34:02 2020
+
+@author: rinorcakaj
+Task 4
+"""
+
+#imports
+import math as m
+import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  
+from tqdm import tqdm
+
+#data
+gmm_data_points = np.loadtxt("dataSets/gmm.txt")
+N = len(gmm_data_points)
+#initialization
+gauss_1 = [np.array([0,0]),np.matrix([[1,0], [0,1]]),0.25] # mu , sigma, pi
+gauss_2 = [np.array([1,0]),np.matrix([[1,0], [0,1]]),0.25]
+gauss_3 = [np.array([2,0]),np.matrix([[1,0], [0,1]]),0.25]
+gauss_4 = [np.array([3,0]),np.matrix([[1,0], [0,1]]),0.25]
+gauss1 = [gauss_1, gauss_2, gauss_3, gauss_4]
+
+
+
+def log_likehood_gaussian(data, gauss):
+    result = 0
+    for i in range(len(data)):
+        result += np.log(gauss[0][2]*gaussian_density_function(data[i], gauss[0][0], gauss[0][1]) + gauss[1][2]*gaussian_density_function(data[i], gauss[1][0], gauss[1][1]) + gauss[2][2]*gaussian_density_function(data[i], gauss[2][0], gauss[2][1]) + gauss[3][2]*gaussian_density_function(data[i], gauss[3][0], gauss[3][1]))
+    return result
+
+
+# evaluate gaussian function
+def gaussian_density_function(x, mu, sigma):
+    x = np.array(x)
+    c1 = 1 / np.sqrt((2*np.pi)**2 * np.linalg.det(sigma))
+    c2 = np.exp(-0.5 * np.matmul((x - mu), np.matmul(np.linalg.inv(sigma),(x - mu)).T))
+    y = c1 * c2
+    return y.item()
+
+#e step
+def e_step(gauss):
+    alpha = np.zeros((N, 4)) #4 wegen 4 versch. gaußverteilungen
+    for j in range(4):
+        for i in range(N):
+            q = 0
+            for p in range(4):
+                q = q + gauss[p][2] * gaussian_density_function(gmm_data_points[i], gauss[p][0], gauss[p][1])
+            alpha[i,j] = (gauss[j][2] * gaussian_density_function(gmm_data_points[i], gauss[j][0], gauss[j][1]))/q
+    return alpha
+
+#m step
+def m_step(gauss, alpha):
+    for j in range(4):
+        # mu new
+        
+        # caclulate N_j
+        N_j = 0
+        for p in range(N):
+            N_j = N_j + alpha[p,j]
+            
+        # calculate mu
+        gauss[j][0] = np.array([0,0])
+        for i in range(N):
+            gauss[j][0] = gauss[j][0] + alpha[i,j]*gmm_data_points[i]
+        gauss[j][0] = gauss[j][0] / N_j
+        
+        # sigma new
+        gauss[j][1] = np.matrix([[0,0],[0,0]])
+        for i in range(N):
+            gauss[j][1] = gauss[j][1] + alpha[i,j]*np.outer(gmm_data_points[i] - gauss[j][0],gmm_data_points[i] - gauss[j][0])
+        gauss[j][1] = gauss[j][1] / N_j
+        # pi new
+        gauss[j][2] = N_j / N
+    return gauss
+        
+            
+            
+        
+
+if __name__ == "__main__":
+    for i in range(30):
+        alpha = e_step(gauss1)
+        gauss1 = m_step(gauss1, alpha)
+        result = log_likehood_gaussian(gmm_data_points,gauss1)
+        print(result)
+    
+    gauss = gauss1
+        
+    delta = 0.25
+    x = np.arange(-5.0, 5.0, delta)
+    y = np.arange(-5.0, 5.0, delta)
+    xx,yy = np.meshgrid(x,y)
+    height = np.zeros((len(xx), len(xx)))
+    height2 = np.zeros((len(xx), len(xx)))
+    height3 = np.zeros((len(xx), len(xx)))
+    height4 = np.zeros((len(xx), len(xx)))
+    for i in range(len(xx)):
+        for j in range(len(xx)):
+            height[i,j] = gaussian_density_function([xx[i,j], yy[i,j]], gauss[0][0], gauss[0][1])
+            height2[i,j] = gaussian_density_function([xx[i,j], yy[i,j]], gauss[1][0], gauss[1][1])
+            height3[i,j] = gaussian_density_function([xx[i,j], yy[i,j]], gauss[2][0], gauss[2][1])
+            height4[i,j] = gaussian_density_function([xx[i,j], yy[i,j]], gauss[3][0], gauss[3][1])
+    
+    plt.scatter(gmm_data_points[:,0], gmm_data_points[:,1], color="black")
+    plt.contour(xx,yy,height, colors ='red')
+    plt.contour(xx,yy,height2, colors = 'blue')
+    plt.contour(xx,yy,height3, colors = 'yellow')
+    plt.contour(xx,yy,height4, colors = 'green')
+    plt.show()
+    
